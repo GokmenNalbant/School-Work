7b8627c4d0ea15f287cbe73e2f926ecba0c6cc6c
GokmenNalbant <62446275+GokmenNalbant@users.noreply.github.com>
2020-04-03 11:58:12 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 790
+        self.back_ground_color = (80,20,20)
+
+        self.back_ground_color2 = (0,255,20)
